a7f854d0d9e89d2117bf465b24a4ed5c95c8a714
Melli11 <mingerez@gmail.com>
2022-05-11 12:24:27 -0300

    Avances Parcial

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,106 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- 1. Modelar a los animales: escribir un sinónimo de tipo y definir algunos ejemplos de animales
+-- como constantes. De un animal se sabe su coeficiente intelectual (un número), su especie (un
+-- string) y sus capacidades (strings).
+
+data Animal = Animal {
+    coeficienteIntelectual :: Number,
+    especie :: String,
+    capacidades :: [String]
+}deriving(Show ,Eq)
+
+-- ejemplos de animales
+perro = Animal 30 "perro" ["Ladrar","Jugar"]
+perroParlante = Animal 30 "perro" ["hablar","Jugar"]
+tigre = Animal 61 "tigre" ["Veloz","Camuflar","hablar"]
+
+
+ratonCrack = Animal 101 "raton" ["Romper las bolas"]
+ratonFalladito = Animal 99 "raton" ["Fallado nomas"]
+
+elefante = Animal 300 "elefante" ["Inteligente"]
+
+-- 2. Transformar a un animal de laboratorio:
+-- ● inteligenciaSuperior: Incrementa en n unidades su coeficiente intelectual
+
+inteligenciaSuperior :: Animal -> Number -> Animal
+inteligenciaSuperior unAnimal cantidadUnidades = unAnimal { coeficienteIntelectual = coeficienteIntelectual unAnimal + cantidadUnidades}
+
+-- ● pinkificar: quitarle todas las habilidades que tenía
+pinkificar :: Animal -> Animal
+pinkificar unAnimal = unAnimal { capacidades = []}
+
+-- ● superpoderes:  le da habilidades nuevas
+    -- ○ En caso de ser un elefante: le da la habilidad “no tenerle miedo a los ratones”
+    -- ○ En caso de ser un ratón con coeficiente intelectual mayor a 100: le agrega la
+    -- habilidad de “hablar”.
+    -- ○ Si no, lo deja como está.
+
+superpoderes :: Animal -> Animal
+superpoderes (Animal iq "elefante" capacidades ) = Animal iq "elefante" ("no tenerle miedo a los ratones": capacidades)
+
+superpoderes (Animal iq "raton" capacidades )
+        |   siTieneIQMayorA 100 (Animal iq "raton" capacidades ) = Animal iq "raton" ("hablar": capacidades )
+        |   otherwise = Animal iq "raton" capacidades
+
+
+-- superpoderes ratonCrack Animal {coeficienteIntelectual = 101, especie = "raton", capacidades = ["hablar","Romper las bolas"]}
+-- superpoderes ratonFalladito  Animal {coeficienteIntelectual = 99, especie = "raton", capacidades = ["Fallado nomas"]}
+
+siTieneIQMayorA :: Number -> Animal -> Bool
+siTieneIQMayorA iQ = (>iQ).coeficienteIntelectual
+
+
+-- 3. Los científicos muchas veces desean saber si un animal cumple ciertas propiedades, porque
+-- luego las usan como criterio de éxito de una transformación. Desarrollar los siguientes criterios:
+
+-- ● antropomórfico: si tiene la habilidad de hablar y su coeficiente es mayor a 60
+
+antropomórfico :: Animal -> Bool
+antropomórfico unAnimal  = siTieneIQMayorA  60 unAnimal && tieneLaHabilidad "hablar" unAnimal --mejorar
+
+tieneLaHabilidad :: String -> Animal -> Bool
+tieneLaHabilidad habilidad = elem habilidad . capacidades
+
+-- noTanCuerdo: si tiene más de dos habilidades debe hacer sonidos pinkiescos. Hacer una
+-- función pinkiesco, que significa que la habilidad empieza con “hacer ”, y luego va seguido
+-- de una palabra "pinkiesca", es decir, con 4 letras o menos y al menos una vocal
+
+-- noTanCuerdo unAnimal 
+--     -- |   tieneMasDe_N_habilidades 2 =  agregarHacerSonidosPinkiescos unAnimal 
+--     |   otherwise = unAnimal
+tieneMasDe_N_habilidades :: Number -> Animal -> Bool
+tieneMasDe_N_habilidades cantidad = (>cantidad ).length.capacidades
+
+
+-- agregarHacerSonidosPinkiescos unAnimal = unAnimal{ capacidades = capacidades unAnimal ++ "hacer" } 
+-- pinkiesco palabra = palabra == "hacer" ++ palabraPinkiesca otraPalabra
+-- pinkiesco :: String -> Bool
+
+pinkiesco :: String -> Bool
+pinkiesco palabra = empiezaConPalabraHAcer (take 6 palabra)  && palabraPinkiesca (drop 6 palabra)  
+
+empiezaConPalabraHAcer ::String -> Bool
+empiezaConPalabraHAcer palabra =  palabra == "hacer "  
+
+palabraPinkiesca :: String -> Bool
+palabraPinkiesca  palabra = ((<=4) . length) palabra && tieneAlMenosUnaVocal palabra
+
+esUnaVocal :: Char -> Bool
+esUnaVocal letra =  letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'
+
+tieneAlMenosUnaVocal :: String -> Bool
+tieneAlMenosUnaVocal = any esUnaVocal
+
+-- 4. Los científicos construyen experimentos: un experimento se compone de un conjunto de
+-- transformaciones sobre un animal, y un criterio de éxito. Se pide:
+-- ● Modelar a los experimentos: dar un sinónimo de tipo.
+-- ● Desarollar experimentoExitoso: Dado un experimento y un animal, indica si al aplicar
+-- sucesivamente todas las transformaciones se cumple el criterio de éxito.
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 module Spec where
 import PdePreludat
 import Library
@@ -5,7 +6,45 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+    suiteDeTestsDeParte1
+    suiteDeTestsDeParte2
+    suiteDeTestsDeParte3
+suiteDeTestsDeParte1 = describe "Punto 1 : Modelando datos" $ do
+    let elefante = Animal 300 "elefante" ["Inteligente"]
+    describe "Modelos de datos " $ do
+        it "Dado un animal de iq 100 que es un elefante y tiene como parte de sus habilidades ser Inteligente " $ do
+            elefante `shouldBe` Animal 300 "elefante" ["Inteligente"]
+
+suiteDeTestsDeParte2 = describe "Punto 2: Transformaciones" $ do 
+    let perro = Animal 30 "perro" ["Ladrar","Jugar"]
+    let tigre = Animal 50 "tigre" ["Veloz","Camuflar"]
+    let ratonCrack = Animal 101 "raton" ["Romper las bolas"]
+    let ratonFalladito = Animal 99 "raton" ["Fallado nomas"]
+    describe "Inteligencia Superior" $ do
+        it "Dado un animal incremento su inteligencia en n unidades" $ do
+            inteligenciaSuperior perro 20 `shouldBe` Animal {coeficienteIntelectual = 50, especie = "perro", capacidades = ["Ladrar","Jugar"]}
+    describe "Pinkificar" $ do
+        it "Dado un animal le saco todas sus habilidades" $ do
+            pinkificar perro `shouldBe` Animal {coeficienteIntelectual = 30, especie = "perro", capacidades = []}
+    describe "Superpoderes" $ do
+        it "Dado un animal que es un elefante le agrego la habilidad no tenerle miedo a los ratones" $ do
+            superpoderes  elefante `shouldBe` Animal 300 "elefante" ["no tenerle miedo a los ratones","Inteligente"]
+        it "Dado un animal que es un raton con iq MAYOR a 100 le agrego la habilidad de hablar " $ do
+            superpoderes ratonCrack `shouldBe` Animal {coeficienteIntelectual = 101, especie = "raton", capacidades = ["hablar","Romper las bolas"]}
+        it "Dado un animal que es un raton con iq MENOR a 100 lo dejo como está " $ do
+            superpoderes ratonFalladito `shouldBe` Animal 99 "raton" ["Fallado nomas"]
+
+suiteDeTestsDeParte3 = describe "Punto 3: Criterios" $ do 
+    let perroParlante = Animal 30 "perro" ["hablar","Jugar"]
+    let tigre = Animal 61 "tigre" ["Veloz","Camuflar","hablar"]
+    
+    describe "Antropomorfico" $ do
+        it "Dado un animal que tiene en su reportorio de capacidades hablar, entonces es parlante " $ do
+            tieneLaHabilidad "hablar" perroParlante `shouldBe` True
+        it "Dado un animal que puede hablar pero tiene Iq menor a 60 entonces NO es antropomorfico " $ do
+            antropomórfico perroParlante `shouldBe` False
+        it "Dado un animal que puede hablar pero y Iq mayor a 60 entonces SI es antropomorfico " $ do
+            antropomórfico tigre `shouldBe` True
+    
+
 
